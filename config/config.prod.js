91d23d57e2d9dfe49fdf551b198e272d0647f5e8
wangle <wangle@nibirutech.com>
2018-11-13 18:31:48 +0800

    .

--- config/config.prod.js
+++ config/config.prod.js
@@ -1,6 +1,6 @@
 exports.mongoose = {
     client : {
-        url : 'mongodb://<user>:<password>@127.0.0.1:27017/ant_blog',
+        url : 'mongodb://<user>:<password>@127.0.0.1:27017/ant_blog',// user: 数据库的用户名 password: 密码
         options : {},
     },
 }
