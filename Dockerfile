d1bcbf07cf238204bbb17848cf36fc4606f62c16
wangle <wangle@nibirutech.com>
2018-11-15 13:58:28 +0800

    .

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,5 @@ FROM node:10.1.0
 COPY . /app
 WORKDIR /app
 RUN npm install --registry=https://registry.npm.taobao.org
-EXPOSE 3000
+EXPOSE 7001
 CMD npm start
--- config/config.prod.js
+++ config/config.prod.js
@@ -1,6 +1,9 @@
+// mongodb用户名密码配置文件需要自己建，我是在服务器上自己建的
+const mongoConfig = require("./mongoConfig")
 exports.mongoose = {
     client : {
-        url : 'mongodb://<user>:<password>@127.0.0.1:27017/ant_blog',// user: 数据库的用户名 password: 密码.
+        // 这里需要连mongo因为mongo是用docker启的所以用ip链接，还没研究到更好的方式
+        url : 'mongodb://{mongoConfig.user}:{mongoConfig.password}@120.77.219.106:27017/ant_blog',// user: 数据库的用户名 password: 密码.
         options : {},
     },
 }
