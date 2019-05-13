97ba72de1399bb8219d3f63ed51bc97db9e36882
wangle <2929712050@qq.com>
2019-05-13 10:36:52 +0800

    .

--- app/controller/article.js
+++ app/controller/article.js
@@ -4,23 +4,23 @@ const {error_001, error_002} = require("../common/common")
 
 class Article extends Controller {
     // create tag
-    async create () {
+    async create() {
         const {ctx, service} = this
         let validator = struct({
-            title : 'string',
-            tag : 'string',
-            markdownValue : 'string?',
-            htmlValue : 'string?',
-            publishAt : 'number',
-            category : 'string',
-            publishStatus : struct.enum([ '1', '2' ])
+            title: 'string',
+            tag: 'string',
+            markdownValue: 'string?',
+            htmlValue: 'string?',
+            publishAt: 'number',
+            category: 'string',
+            publishStatus: struct.enum(['1', '2'])
         })
         try {
             validator(ctx.request.body)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
-        
+
         try {
             let content = {...ctx.request.body}
             content.creator = ctx.id
@@ -31,45 +31,45 @@ class Article extends Controller {
             const article = await service.article.create(content)
             return ctx.helper.success(ctx, article)
         } catch (err) {
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async getArticle () {
+
+    async getArticle() {
         const {ctx, service} = this
         let validator = struct({
-            _id : 'string'
+            _id: 'string'
         })
-        
+
         try {
             validator(this.ctx.params)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
-        
+
         try {
             await service.article.updateViewCount(this.ctx.params._id)
             const article = await service.article.findOne(this.ctx.params)
             return ctx.helper.success(ctx, article)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async getArticles () {
+
+    async getArticles() {
         const {ctx, service} = this
         let validator = struct({
-            pageSize : 'string?',
-            pageLimit : 'string?',
-            publishStatus : 'string?'
+            pageSize: 'string?',
+            pageLimit: 'string?',
+            publishStatus: 'string?'
         })
         try {
             validator(ctx.request.query)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
-        
+
         try {
             const article = await service.article.find(ctx.request.query)
             for (let val of article.article) {
@@ -80,35 +80,35 @@ class Article extends Controller {
             return ctx.helper.success(ctx, article)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async findByIdAndUpdate () {
+
+    async findByIdAndUpdate() {
         const {ctx, service} = this
         const validatorParams = struct({
-            _id : 'string'
+            _id: 'string'
         })
         const validatorBody = struct({
-            title : 'string?',
-            tag : 'string?',
-            markdownValue : 'string?',
-            htmlValue : 'string?',
-            publishAt : 'number?',
-            publishStatus : struct.enum([ '1', '2' ]),
-            category : 'string',
+            title: 'string?',
+            tag: 'string?',
+            markdownValue: 'string?',
+            htmlValue: 'string?',
+            publishAt: 'number?',
+            publishStatus: struct.enum(['1', '2']),
+            category: 'string',
         })
         try {
             validatorParams(ctx.params)
             validatorBody(ctx.request.body)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         try {
             const updatingContent = {
                 ...ctx.request.body,
-                updatedAt : Date.now(),
-                status : '1'
+                updatedAt: Date.now(),
+                status: '1'
             }
             let tag_detail = null, category_detail = null
             if (ctx.request.body.tag) {
@@ -129,106 +129,106 @@ class Article extends Controller {
             return ctx.helper.success(ctx, article)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async deleteArticle () {
+
+    async deleteArticle() {
         const {ctx, service} = this
         const validator = struct({
-            id : 'string'
+            id: 'string'
         })
         try {
             validator(ctx.params)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
-        
+
         try {
             const updatingContent = {
-                deletedAt : Date.now(),
-                status : '2'
+                deletedAt: Date.now(),
+                status: '2'
             }
             const article = await service.article.findByIdAndUpdate(ctx.params.id, updatingContent)
-            
+
             return ctx.helper.success(ctx, article)
         } catch (err) {
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async findByTag () {
+
+    async findByTag() {
         const {ctx, service} = this
         const validatorParams = struct({
-            tag : 'string'
+            tag: 'string'
         })
         try {
             validatorParams(ctx.params)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         try {
             const article = await service.article.findByTag(ctx.params.tag)
             return ctx.helper.success(ctx, article)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async findByCategory () {
+
+    async findByCategory() {
         const {ctx, service} = this
         const validatorParams = struct({
-            category : 'string',
+            category: 'string',
         })
         const validatorQuery = struct({
-            pageSize : 'string?',
-            pageLimit : 'string?'
+            pageSize: 'string?',
+            pageLimit: 'string?'
         })
         try {
             validatorParams(ctx.params)
             validatorQuery(ctx.query)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         try {
             const article = await service.article.findByCategory(ctx.params.category, ctx.query.pageSize, ctx.query.pageLimit)
             return ctx.helper.success(ctx, article)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async archives () {
+
+    async archives() {
         const {ctx, service} = this
         try {
             const _archives = await service.article.aggregateArchives()
             return ctx.helper.success(ctx, _archives)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async findByArchive () {
+
+    async findByArchive() {
         const {ctx, service} = this
         const validatorParams = struct({
-            timeline : 'string'
+            timeline: 'string'
         })
         const validatorQuery = struct({
-            pageSize : 'string?',
-            pageLimit : 'string?'
+            pageSize: 'string?',
+            pageLimit: 'string?'
         })
         try {
             validatorParams(ctx.params)
             validatorQuery(ctx.query)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         const date = (new Date(ctx.params.timeline))
         if (typeof date.getTime() !== 'number') {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         let start = date.getTime(),
             month = date.getMonth() === 11 ? 1 : date.getMonth() + 2,
@@ -240,7 +240,7 @@ class Article extends Controller {
             return ctx.helper.success(ctx, _archives)
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
 }
--- app/controller/user.js
+++ app/controller/user.js
@@ -6,46 +6,46 @@ const sha1 = require('sha1')
 const rand = require('csprng')
 
 class User extends Controller {
-    
-    async getUserList () {
+
+    async getUserList() {
         const {ctx, service} = this
         try {
             const user = await service.user.find()
             if (!user) {
-                return ctx.helper.error(ctx, error_003[ 0 ], error_003[ 1 ])
+                return ctx.helper.error(ctx, error_003[0], error_003[1])
             } else {
                 return ctx.helper.success(ctx, user)
             }
         } catch (e) {
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async getUserInfo () {
+
+    async getUserInfo() {
         const {ctx, service} = this
         try {
             const user = await service.user.findById(ctx.id)
             if (!user) {
-                return ctx.helper.error(ctx, error_003[ 0 ], error_003[ 1 ])
+                return ctx.helper.error(ctx, error_003[0], error_003[1])
             } else {
                 return ctx.helper.success(ctx, user)
             }
         } catch (e) {
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async editUserInfo () {
+
+    async editUserInfo() {
         const {ctx, service} = this
         const validator = struct({
-            name : 'string',
-            password : 'string?',
-            id : 'string'
+            name: 'string',
+            password: 'string?',
+            id: 'string'
         })
         try {
             validator(ctx.request.body)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
         try {
             const {name, password} = ctx.request.body
@@ -62,43 +62,43 @@ class User extends Controller {
         }
         catch (e) {
             console.log(e)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
-    async login () {
+
+    async login() {
         const {ctx, service} = this
         const validator = struct({
-            name : 'string',
-            password : 'string'
+            name: 'string',
+            password: 'string'
         })
-        
+
         try {
             validator(ctx.request.body)
         } catch (err) {
-            return ctx.helper.error(ctx, error_002[ 0 ], error_002[ 1 ])
+            return ctx.helper.error(ctx, error_002[0], error_002[1])
         }
-        
+
         try {
-            const user_f = await service.user.findByName({name : ctx.request.body.name})
+            const user_f = await service.user.findByName({name: ctx.request.body.name})
             ctx.request.body.password = sha1(ctx.request.body.password + user_f.salt)
             const user = await service.user.findOne(ctx.request.body)
             if (!user) {
-                return ctx.helper.error(ctx, user_001[ 0 ], user_001[ 0 ])
+                return ctx.helper.error(ctx, user_001[0], user_001[0])
             } else if (user.role !== 1) {
-                return ctx.helper.error(ctx, error_004[ 0 ], error_004[ 1 ])
+                return ctx.helper.error(ctx, error_004[0], error_004[1])
             } else {
-                const token = service.user.createToken({id : user.id})
-                await service.user.updateById(user.id, {updatedAt : Date.now()})
+                const token = service.user.createToken({id: user.id})
+                await service.user.updateById(user.id, {updatedAt: Date.now()})
                 return ctx.helper.success(ctx, {token})
             }
-            
+
         } catch (err) {
             console.log(err)
-            return ctx.helper.error(ctx, error_001[ 0 ], error_001[ 1 ])
+            return ctx.helper.error(ctx, error_001[0], error_001[1])
         }
     }
-    
+
 }
 
 module.exports = User
